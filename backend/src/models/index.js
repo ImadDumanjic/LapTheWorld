e454b2a4eafb7c714fcc4139a21aa88c38da6cd1
Imad Dumanjić <imad.dumanjic@stu.ibu.edu.ba>
2026-03-28 20:22:14 +0100

    Add PostgreSQL support with Sequelize and update server initialization

---
+++ backend/src/models/index.js
@@ -0,0 +1,7 @@
+import sequelize from "../config/database";
+
+const db = {};
+
+db.Sequelize = sequelize;
+
+export default db;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,22 @@
 import dotenv from 'dotenv';
 import app from './app.js';
+import sequelize from './config/database.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function initializeDatabase() {
+  try{
+    await sequelize.authenticate();
+    console.log('Connection to the database has been established successfully.');
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error){
+    console.error('Unable to connect to the database:', error.message);
+  }
+}
+
+initializeDatabase();
